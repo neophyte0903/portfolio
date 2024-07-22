25077a73a49818b697930c3e50c65b8c506cca80
Gagan Deep Singh <gagan@Gagans-Laptop.local>
2024-07-22 17:06:04 +0530

    Updates

---
+++ precache-manifest.d9fcc27ebdbe6fcce4584ea3f73aa52a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ec5a087c57e23390dbee3fc9b7807de",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "f71f369c9b7d684ba9e4",
+    "url": "/portfolio/static/css/main.1624d5f1.chunk.css"
+  },
+  {
+    "revision": "c0ee305eb3145ecf5a2b",
+    "url": "/portfolio/static/js/2.07b41d29.chunk.js"
+  },
+  {
+    "revision": "f71f369c9b7d684ba9e4",
+    "url": "/portfolio/static/js/main.14f07823.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  }
+]);
